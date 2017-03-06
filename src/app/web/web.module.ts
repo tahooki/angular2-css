798ce8971dfdf9fcc616ecde67542f2a8b3d0838
tahooki <tahooki@naver.com>
2017-03-06 18:40:06 +0900

    chore: app에 header 추가

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BootstrapModule } from './routes/bootstrap/bootstrap.module';
 import { IndexModule } from './routes/index/index.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   imports: [
     IndexModule,
     BootstrapModule
-  ]
+  ],
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class WebModule {
 }
