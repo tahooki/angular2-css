873081b3cee468dcebc3f39cfdc4529a582dc345
tahooki <tahooki@naver.com>
2017-03-10 00:55:31 +0900

    chore: 스터디 페이지 추가 - study.component.*

---
+++ src/app/web/routes/study/study.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { StudyComponent } from './study.component';
+
+describe('StudyComponent', () => {
+  let component: StudyComponent;
+  let fixture: ComponentFixture<StudyComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudyComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/web/routes/study/study.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudyRoutingModule } from './study-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StudyRoutingModule
+  ],
+  declarations: []
+})
+export class StudyModule { }
