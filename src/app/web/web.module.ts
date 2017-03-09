7790e01e3dcb6951f97008b7ee0511f968913b52
tahooki <tahooki@naver.com>
2017-03-10 00:57:31 +0900

    chore: slide 모듈 추가 - web.module.ts - package.json

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BootstrapModule } from './routes/bootstrap/bootstrap.module';
 import { IndexModule } from './routes/index/index.module';
+import { StudyComponent } from './routes/study/study.component';
+import { StudyModule } from './routes/study/study.module';
+import { SlidesModule } from 'ng2-slides/dist';
 
 @NgModule({
   imports: [
     IndexModule,
-    BootstrapModule
+    BootstrapModule,
+    SlidesModule,
+    StudyModule
   ],
-  declarations: [],
+  declarations: [StudyComponent],
   exports: []
 })
 export class WebModule {
