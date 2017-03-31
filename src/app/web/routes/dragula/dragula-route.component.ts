06ceca74677b5fc9817c631d1c4876db2506b922
tahooki <tahooki@naver.com>
2017-03-31 13:55:47 +0900

    fix: 드라귤러 페이지 해더 수정, 옵션삭제

--- src/app/web/routes/dragula/dragula-route.component.ts
+++ src/app/web/routes/dragula/dragula-route.component.ts
@@ -20,16 +20,6 @@ export class DragulaRouteComponent implements OnInit {
     ignoreInputTextSelection: true     // allows users to select input text, see details below
   }
 
-  options2: any = {
-    direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
-    copy: false,                       // elements are moved by default, not copied
-    copySortSource: false,             // elements in copy-source containers can be reordered
-    revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
-    removeOnSpill: false,              // spilling will `.remove` the element, if this is true
-    mirrorContainer: document.body,    // set the element that gets mirror elements appended
-    ignoreInputTextSelection: true     // allows users to select input text, see details below
-  }
-
   isBasket: boolean = false;
 
   constructor(private _dragulaService: DragulaService) {
@@ -68,7 +58,8 @@ export class DragulaRouteComponent implements OnInit {
   private onDrop(args) {
     let [e, el] = args;
     // do something
-    if(this.isBasket) {
+    if(el && el.className === 'basket') {
+      console.log('onDrop !! ', el.parent);
       var nodes = document.getElementsByClassName("container2");
       nodes[0].appendChild(e);
     }
